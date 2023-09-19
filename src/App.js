31ffc7b28d52038615df470b63251f16416f8765
donymyounglee <pajang1515@daum.net>
2023-09-19 14:08:46 +0900

    init

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import {BrowserRouter} from "react-router-dom";
+import CustomRouter from "./routes/CustomRouter";
+
+function App() {
+  return (
+    <CustomRouter/>
+  );
+}
+
+export default App;
